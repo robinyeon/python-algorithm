1123e038669d8ec5a57fb2d7c16ed3571ce2475a
robinyeon <robin.yeon@gmail.com>
2022-09-26 21:11:43 +0900

    그리디, 구현 문제풀이

--- BOJ/2798.py
+++ BOJ/2798.py
@@ -3,8 +3,6 @@ import itertools
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
 
-sum_list = []
-
 max = 0
 for x in itertools.combinations(cards, 3):
     if max < sum(x) <= m:
