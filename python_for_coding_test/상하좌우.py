bcac06533f13772a31aa2e36689ad326dd344f7f
robinyeon <robin.yeon@gmail.com>
2022-09-30 11:22:46 +0900

    구현 실전문제

---
+++ python_for_coding_test/상하좌우.py
@@ -0,0 +1,22 @@
+n = int(input())
+directions = input().split()
+
+x, y = 1, 1
+
+# dx, dy를 작성할때 단순히 함수 그래프로 속단하지 말고 문제대로 따라가기
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+moves = ['L', 'R', 'U', 'D']
+
+
+for direction in directions:
+    for i in range(len(moves)):
+        if direction == moves[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    # continue로 영역 벗어났을 시 무시
+    if nx > n or nx < 1 or ny > n or ny < 1:
+        continue
+    x, y = nx, ny
+
+print(x, y)
